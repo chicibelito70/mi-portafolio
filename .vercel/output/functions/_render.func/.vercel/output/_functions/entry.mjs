d9878c7e644dbf2a2ea34a0b39de7613b9e25f59
chicibelito70 <chicibelito07@gmail.com>
2024-08-30 17:01:49 -0400

    Fix: cambios

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_B0w5rLIs.mjs';
+import { manifest } from './manifest_DUD9wjdO.mjs';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "757e6caa-3e63-4c32-a98c-e788c25916ef",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
